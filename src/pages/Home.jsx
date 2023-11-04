a0908cc77ea7f63132e665f7a79dfa389ec45a3e
TinInvisible <botomtinlon@gmail.com>
2023-11-04 13:12:25 +0700

    change homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,30 +5,30 @@ import Filter from "../components/Filter";
 import Jobs from "../components/Jobs";
 
 function Home() {
-    const [jobs, setJobs] = useState([]);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [filter, setFilter] = useState({});
+    // const [jobs, setJobs] = useState([]);
+    // const [pageNumber, setPageNumber] = useState(1);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [filter, setFilter] = useState({});
 
-    useEffect(() => {
-        const fetchJobs = async () => {
-            const response = await getJobs({ page: currentPage, ...filter });
-            setJobs(response.data.posts);
-            setPageNumber(response.data.totalPages);
-        };
+    // useEffect(() => {
+    //     const fetchJobs = async () => {
+    //         const response = await getJobs({ page: currentPage, ...filter });
+    //         setJobs(response.data.posts);
+    //         setPageNumber(response.data.totalPages);
+    //     };
 
-        fetchJobs();
-    }, [filter, currentPage]);
+    //     fetchJobs();
+    // }, [filter, currentPage]);
 
-    const handleFilter = (filter) => {
-        setFilter(filter);
-        setCurrentPage(1);
-    };
+    // const handleFilter = (filter) => {
+    //     setFilter(filter);
+    //     setCurrentPage(1);
+    // };
 
     return (
-        <div name="home" className="w-full h-full text-gray-300 bg-[#393E46]">
+        <div name="home" className="w-full h-screen text-gray-300 bg-[#393E46]">
             <div className="pt-[120px] pb-[50px] max-w-[1100px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-                <div className="pb-4">
+                {/* <div className="pb-4">
                     <div className="flex justify-between">
                         <div>
                             <p className="text-4xl font-bold inline text-[#00ADB5] border-b-4 border-pink-600">Jobs</p>
@@ -36,9 +36,10 @@ function Home() {
                         </div>
                         <Filter onFilter={handleFilter} />
                     </div>
-                </div>
+                </div> */}
 
-                <Jobs jobs={jobs} currentPage={currentPage} pageNumber={pageNumber} setCurrentPage={setCurrentPage} />
+                {/* <Jobs jobs={jobs} currentPage={currentPage} pageNumber={pageNumber} setCurrentPage={setCurrentPage} /> */}
+                <p className="text-4xl font-bold inline text-[#00ADB5] border-b-4 border-pink-600 text-center"> This is Home Page</p>
             </div>
         </div>
     );

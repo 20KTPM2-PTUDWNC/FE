2130eebf45f34dc1946d9f17358d25557bce3a61
LeThuyNga1606 <lethuynga1662002@gmail.com>
2023-12-15 14:10:09 +0700

    View, sort, filter classes

--- src/pages/admin/Class.admin.js
+++ src/pages/admin/Class.admin.js
@@ -1,13 +1,16 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getAllClass } from "../../api/class/class.api.js";
+import { getAllClass, getAllClassById } from "../../api/class/class.api.js";
 import { getCookies, getUser } from "../../features/user";
 import Logo from "../../assets/cover.jpg";
 import Cookies from "universal-cookie";
 import { FaEye } from "react-icons/fa";
 import axios from "axios";
 import DataTable from "react-data-table-component"
+import { getProfile } from "../../api/user/user.api";
+import { showMemberList } from "../../api/class/class.api.js";
+
 function ClassAdminPage() {
     const navigate = useNavigate();
     const user = getUser();
@@ -16,17 +19,16 @@ function ClassAdminPage() {
     const [showJoinClass, setShowJoinClass] = useState(false);
     const [listClass, setListClass] = useState([]);
     const [images, setImages] = useState([])
-    const [isAddClass, setIsAddClass] = useState(0)
-    const [records, setRecords] = useState([])
+    const [memberCounts, setMemberCounts] = useState({});
 
-    // useEffect(() => {
-    //     if (!user) {
-    //         navigate("/signin")
-    //     }
-    //     else {
-    //         cookie.set('token', getCookies(), { path: `/v1/className/getAllClassById` });
-    //     }
-    // }, [])
+    useEffect(() => {
+        if (!user) {
+            navigate("/signin")
+        }
+        else {
+            cookie.set('token', getCookies(), { path: `/v1/className/getAllClass` });
+        }
+    }, [])
 
     const column = [
         {
@@ -40,33 +42,37 @@ function ClassAdminPage() {
         },
         {
             name: "Description",
-            selector: row => row.email,
+            selector: row => row.subject,
             sortable: true
         },
         {
-            name: "Code"
+            name: "Code",
+            selector: row => row.code,
         },
         {
-            name: "Author"
+            name: "Author",
+            selector: row => row.author,
         },
         {
-            name: "Status"
+            name: "Status",
+            selector: row => row.status,
+            cell: row => (
+                <span style={{ color: row.status === 0 ? "green" : "red" }}
+                    onClick={() => handleStatusChange(row._id)}>
+                    {row.status === 0 ? "Active" : "Inactive"}
+                </span>
+            )
         },
         {
-            name: "Number of members"
+            name: "Number of members",
+            cell: row => <span>{memberCounts[row._id] || 0}</span>
         },
         {
             name: "List of members",
-            cell: row => <FaEye onClick={() => handleEyeClick(row.id)} />
+            cell: row => <FaEye onClick={() => handleEyeClick(row._id)} />
         },
     ]
-    useEffect(() => {
-        if (showAddNewClass || showJoinClass) {
-            document.body.classList.add("overflow-hidden");
-        } else {
-            document.body.classList.remove("overflow-hidden");
-        }
-    }, [showAddNewClass, showJoinClass]);
+
 
     useEffect(() => {
 
@@ -86,31 +92,63 @@ function ClassAdminPage() {
     }
 
     useEffect(() => {
-        // async function fetchClasses() {
-        //     try {
-        //         const response = await getAllClass();
-        //         if (response.status === 200) {
-        //             setListClass(response.data);
-        //             console.log(response.data)
-        //         }
-        //     } catch (error) {
-        //         console.log("Error: ", error);
-        //     }
-        // }
-        // fetchClasses();
-
-        async function fetchData(){
-            axios.get('http://jsonplaceholder.typicode.com/users')
-            .then(res => setRecords(res.data))
-            .then(console.log(records))
-            .catch(err => console.log(err));
+        async function fetchClasses() {
+            try {
+                const response = await getAllClass();
+                if (response.status === 200) {
+                    setListClass(response.data);
+                    console.log(response.data);
+                    listClass.forEach((_class) => {
+                        authorInfor(_class.authorId);
+                        getMemberList(_class._id);
+                    });
+                }
+            } catch (error) {
+                console.log("Error: ", error);
+            }
         }
-        fetchData();
+        fetchClasses();
+        
     }, []);
 
+    async function authorInfor(authorId) {
+        try {
+            const response = await getProfile(authorId);
+
+            if (response.status === 200) {
+                const authorInfo = response.data;
+                setListClass(prevClasses => prevClasses.map(_class => {
+                    if (_class.authorId === authorId) {
+                        _class.author = authorInfo.name;
+                    }
+                    return _class;
+                }));
+            }
+        } catch (error) {
+            console.log("Error123: ", error);
+        }
+    }
+
+    async function getMemberList(classId) {
+        try {
+            const response = await showMemberList(classId);
+
+            if (response.status === 200) {
+                const listMember = response.data;
+                setMemberCounts(prevCounts => ({
+                        ...prevCounts,
+                        [classId]: listMember.students.length + listMember.teachers.length,
+                }));
+            }
+        } catch (error) {
+            console.log("Error123: ", error);
+
+        }
+    }
+
     const handleFilter = (e) => {
-        const newData = records.filter(row => row.name.toLowerCase().includes(e.target.value.toLowerCase()));
-        setRecords(newData);
+        const newData = listClass.filter(row => row.name.toLowerCase().includes(e.target.value.toLowerCase()));
+        setListClass(newData);
     }
 
     const handleEyeClick = (rowId) => {
@@ -118,17 +156,33 @@ function ClassAdminPage() {
         navigate(`/admin/class/member-details/${rowId}`);
     };
 
+    const handleStatusChange = async (classId) => {
+        //try {
+        //   const response = await updateClassStatus(classId);
+        //   if (response.status === 200) {
+            setListClass((prevList) =>
+                prevList.map((item) =>
+                    item._id === classId ? { ...item, status: 1 - item.status } : item
+                )
+            );
+        //}
+        // } catch (error) {
+        //   console.error("Error updating status:", error);
+        // }
+      };
+
     return (
         <>
-            <main className="ml-60 pt-16 max-h-screen overflow-auto">
+            <main className="ml-60 pt-16 h-screen bg-yellow-50 overflow-auto">
                 <div className="px-6 py-8">
-                    <div style={{display: 'flex', justifyContent: 'right'}}>
-                        <input type="text" placeholder="Search..." onChange={handleFilter}></input>
-                    </div>
                     <div className="max-w-5xl mx-auto bg-white rounded-3xl p-8 mb-5">
+                        <div style={{display: 'flex', justifyContent: 'right'}}>
+                            <input type="text" placeholder="Search..." onChange={handleFilter} 
+                            style={{ border: '1px solid black', borderRadius: '4px', padding: '8px' }}></input>
+                        </div>
                         <DataTable
                             columns = {column}
-                            data = {records}
+                            data = {listClass}
                             pagination>
                         </DataTable>
                     </div>
